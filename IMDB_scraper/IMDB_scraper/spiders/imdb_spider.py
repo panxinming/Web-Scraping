898b6ed265ebfda129e171c7ca560f1a878a779f
Xinming Pan <83961129+panxinming@users.noreply.github.com>
2022-01-25 16:34:18 -0800

    Update imdb_spider.py

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -1,9 +1,12 @@
-# to run 
-# scrapy crawl imdb_spider -o movies.csv
-
 import scrapy
 
 class ImdbSpider(scrapy.Spider):
     name = 'imdb_spider'
     
     start_urls = ['https://www.imdb.com/title/tt2250192/?ref_=nv_sr_srsg_0']
+
+    def parse(self, response):
+        filename = f"imdb.html"
+        with open(filename, "wb") as f:
+            f.write(response.body)
+
