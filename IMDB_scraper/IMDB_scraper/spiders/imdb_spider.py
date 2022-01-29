7a32d29badca9b2f87e9e73c667b34fe0e3fc5b4
hhhh <83961129+panxinming@users.noreply.github.com>
2022-01-28 21:52:09 -0800

    Update imdb_spider.py

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -1,23 +1,32 @@
 import scrapy
+import random
 
 class ImdbSpider(scrapy.Spider):
     name = 'imdb_spider'
-    
-    start_urls = ['https://www.imdb.com/title/tt2250192/?ref_=nv_sr_srsg_0']
+    start_urls = ['https://www.imdb.com/title/tt0106145/']
+
 
     def parse(self, response):
-        filename = f"imdb.html"
-        with open(filename, "wb") as f:
-            f.write(response.body)
+        url = start_urls[0] + "fullcredits"
+        yield Request(url, callback = self.parse_full_credits)
+
 
     def parse_full_credits(self, response):
+        actor = [a.attrib["href"] for a in response.css("td.primary_photo a")]
+        i = random.choice(list(range(0,len(actor))))
+        url = "www.imdb.com" + actor[i]
+        yield Request(url, callback = self.parse_actor_page)
+
+
+    def parse_actor_page(self, response):
+        for element in response.css("div#content-2-wide.redesign"):
+            actor_name = element.css("span.itemprop::text").get()
+            TV_Movie = element.css("div.filmo-row a::text").getall()
+            TV_Movie = ",".join(TV_Movie)
+
+            yield {"actor" : actor_name, "movie_or_TV_name" : TV_Movie}
 
 
 
-class parse_full_credits(self, response):
-    name = 'imdb_spider'   
-    start_urls = ['https://www.imdb.com/title/tt2250192/?ref_=nv_sr_srsg_0']
 
-
-for quote in response.css("a.ipc-link.ipc-link--baseAlt.ipc-link--inherit-color"):
     
